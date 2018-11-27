b0f73a146d9565f43e821a312d7e53cf29531820
elisapig <elisa1001940@gmail.com>
2018-11-27 16:21:04 +0800

    List,Recycler

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.user.myapplication;
 
 import android.content.Intent;
+import android.support.v4.widget.ListViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends  BaseActivity {
     private static final int RC_LOGIN = 100;
-    boolean logon = false;
+    boolean login = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView nickText = findViewById(R.id.nickname);
-        nickText.setText(user.getNickname());
-        if(!logon){
+        if(!login){
             Intent intent = new Intent (this,LoginActivity.class);
             startActivityForResult(intent,RC_LOGIN);
-
-
+        //ListView
+        // listView();
+            // RecyclerView
+            RecyclerView recyclerView = findViewById(R.id.recycler);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter();
         }
+
+        //data
+        List<String> fruits = Arrays.asList("水蜜桃","葡萄","芒果");
+        ArrayAdapter adapter =
+                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
+        ListView listView = findViewById(R.id.list) ;
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -29,7 +47,7 @@ public class MainActivity extends  BaseActivity {
             if (resultCode != RESULT_OK) {
                 finish();
             } else {
-                logon = true;
+
               //  String nickname = getSharedPreferences("user", MODE_PRIVATE)
               //          .getString("NICKNMAE", null);
                 //int age = getSharedPreferences("user", MODE_PRIVATE)
@@ -37,9 +55,9 @@ public class MainActivity extends  BaseActivity {
                // int gender = getSharedPreferences("user", MODE_PRIVATE)
                //         .getInt("Gender", 0);
               //  if (nickname == null || age == 0 || gender == 0) {
-                if(user.isValid()){
-                    Intent nick = new Intent(this, NicknameActivity.class);
-                    startActivity(nick);
+                if(!user.isValid()){
+                    Intent nickname = new Intent(this, NicknameActivity.class);
+                    startActivity(nickname);
                 }
             }
         }}
