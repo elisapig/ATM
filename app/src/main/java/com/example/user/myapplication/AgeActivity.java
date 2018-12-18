b93275d624971caf54f5da68ea85c24bcfcf60df
elisapig <elisa1001940@gmail.com>
2018-12-18 16:34:29 +0800

    RecyclerView click on item change age

--- app/src/main/java/com/example/user/myapplication/AgeActivity.java
+++ app/src/main/java/com/example/user/myapplication/AgeActivity.java
@@ -2,11 +2,13 @@ package com.example.user.myapplication;
 
 import android.content.Intent;
 import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
@@ -14,29 +16,33 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class AgeActivity extends BaseActivity {
-    int [] numbers = {19,20,21,22,23};
+    int [] numbers = {16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+    String[] rainbow = null;
+    private EditText edAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        rainbow = getResources().getStringArray(R.array.rainbow);
         RecyclerView recyclerView =findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         AgeAdapter ageadapter = new AgeAdapter();
         recyclerView.setAdapter(ageadapter);
-
+        edAge = findViewById(R.id.ed_age);
 
         }
 
     public void next(View view) {
-        int age = Integer.parseInt(((EditText) findViewById(R.id.ed_age)).getText().toString());
+
       //  getSharedPreferences("user", MODE_PRIVATE)
      //           .edit()
        //         .putInt("AGE", age)
          //       .apply();
-        user.setAge(age);
-        Intent gender = new Intent(this, GenderActivity.class);
-        startActivity(gender);
+       // user.setAge(age);
+      //  Intent gender = new Intent(this, GenderActivity.class);
+       // startActivity(gender);
     }
 
     public void back(View view) {
@@ -51,9 +57,18 @@ public class AgeActivity extends BaseActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull AgeHolder holder, int position) {
+        public void onBindViewHolder(@NonNull AgeHolder holder, final int position) {
         holder.ageText.setText(numbers[position]+"");
+        holder.itemView.setBackgroundColor(numbers[position]%7);
+        holder.ageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("AgeActivity","onClick();"+numbers[position]);
+                edAge.setText(numbers[position]+"");
+            }
+        });
         }
+
 
         @Override
         public int getItemCount() {
--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -26,75 +26,83 @@ public class MainActivity extends  BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView nickText = findViewById(R.id.ed_nickname);
+        nickText.setText(user.getNickname());
         if(!login){
             Intent intent = new Intent (this,LoginActivity.class);
             startActivityForResult(intent,RC_LOGIN);
         //ListView
         // listView();
             // RecyclerView
-          RecyclerView recyclerView = findViewById(R.id.recycler);
-          recyclerView.setHasFixedSize(true);
+            RecyclerView recyclerView = findViewById(R.id.recycler);
+            recyclerView.setHasFixedSize(true);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
             recyclerView.setAdapter(new FruitAdapter());
 
         }
-        class  FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder>{
 
-            @NonNull
-            @Override
-            public FruitAdapter.FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext())
-                        .inflate(android.R.layout.simple_list_item_1,parent,false);
-                        return new FruitViewHolder(view);
-            }
-
-            @Override
-            public void onBindViewHolder(@NonNull FruitAdapter.FruitViewHolder holder, int position) {
-                holder.nameText.setText(fruits.get(position));
-
-            }
-
-            @Override
-            public int getItemCount() {
-                return fruits.size();
-            }
-            class FruitViewHolder extends RecyclerView.ViewHolder{
-                TextView nameText;
-
-                public FruitViewHolder(View itemView) {
-                    super(itemView);
-                    nameText=itemView.findViewById(android.R.id.text1);
-                }
-            }
         }
         //data
 
+
+    class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder> {
+
+        @NonNull
+        @Override
+        public FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new FruitViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull FruitViewHolder holder, int position) {
+            holder.nameText.setText(fruits.get(position));
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return fruits.size();
+
+        }
+
+        class FruitViewHolder extends RecyclerView.ViewHolder {
+            TextView nameText;
+
+            public FruitViewHolder(View itemView) {
+                super(itemView);
+                nameText = itemView.findViewById(android.R.id.text1);
+            }
+        }
+    }
+    private void listView(){
         List<String> fruits = Arrays.asList("水蜜桃","葡萄","芒果");
-        ArrayAdapter adapter =
-                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
+        ArrayAdapter<String> adapter=
+        new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
         ListView listView = findViewById(R.id.list) ;
         listView.setAdapter(adapter);
     }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == RC_LOGIN) {
+                if (resultCode != RESULT_OK) {
+                    finish();
+                } else {
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_LOGIN) {
-            if (resultCode != RESULT_OK) {
-                finish();
-            } else {
-
-              //  String nickname = getSharedPreferences("user", MODE_PRIVATE)
-              //          .getString("NICKNMAE", null);
-                //int age = getSharedPreferences("user", MODE_PRIVATE)
-                 //       .getInt("AGE", 0);
-               // int gender = getSharedPreferences("user", MODE_PRIVATE)
-               //         .getInt("Gender", 0);
-              //  if (nickname == null || age == 0 || gender == 0) {
-                if(!user.isValid()){
-                    Intent nickname = new Intent(this, NicknameActivity.class);
-                    startActivity(nickname);
+                    //  String nickname = getSharedPreferences("user", MODE_PRIVATE)
+                    //          .getString("NICKNMAE", null);
+                    //int age = getSharedPreferences("user", MODE_PRIVATE)
+                    //       .getInt("AGE", 0);
+                    // int gender = getSharedPreferences("user", MODE_PRIVATE)
+                    //         .getInt("Gender", 0);
+                    //  if (nickname == null || age == 0 || gender == 0) {
+                    if (!user.isValid()) {
+                        Intent nickname = new Intent(this, NicknameActivity.class);
+                        startActivity(nickname);
+                    }
                 }
             }
-        }}
-    }
+
+    }}
